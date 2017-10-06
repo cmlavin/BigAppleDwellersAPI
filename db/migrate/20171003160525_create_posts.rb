3482de79398a243eca6056bf5bcb08e24b4bd513
Catherine <cmlavin7@gmail.com>
2017-10-06 19:54:58 -0400

    Set up rack-cors, added controllers, and some edits to db tables.

--- db/migrate/20171003160525_create_posts.rb
+++ db/migrate/20171003160525_create_posts.rb
@@ -5,10 +5,9 @@ class CreatePosts < ActiveRecord::Migration[5.1]
       t.integer :user_id
       t.text :content
       t.integer :likes
+      t.datetime :created_at
+      t.datetime :edited_at
       t.timestamps
     end
   end
 end
-
-#do likes get saved in database or in state?
-#include created_at and edited_at columns
